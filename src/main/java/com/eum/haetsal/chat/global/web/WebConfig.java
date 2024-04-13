30619ec08315d9c212614abc8dcf6f420601f76e
ytjdud <102743371+ytjdud@users.noreply.github.com>
2024-04-14 00:23:25 +0900

    Add Web config: CORS, forwarding swagger-ui

---
+++ src/main/java/com/eum/haetsal/chat/global/web/WebConfig.java
@@ -0,0 +1,27 @@
+package com.eum.haetsal.chat.global.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/swagger-ui/")
+                .setViewName("forward:/swagger-ui/index.html");
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("x-requested-with, Authorization, Content-Type")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
