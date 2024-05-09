0dc7bc71071c98ad61ac1d634f658d7d91277bfb
ytjdud <102743371+ytjdud@users.noreply.github.com>
2024-05-09 15:52:58 +0900

    Chore: set allowed methods of web

--- src/main/java/com/eum/haetsal/chat/global/web/WebConfig.java
+++ src/main/java/com/eum/haetsal/chat/global/web/WebConfig.java
@@ -8,8 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/swagger-ui/")
@@ -18,10 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/chat-service/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .allowedHeaders("*") // "userId", "Authorization" 등..
-                .allowCredentials(true);
+                .allowedOrigins("http://localhost:3000", "https://k-eum2023.web.app")
+                .allowedMethods("GET", "POST", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
 }
