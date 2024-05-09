d1294ddca11021a1c4162a22669a3a31aa2146b9
ytjdud <102743371+ytjdud@users.noreply.github.com>
2024-05-10 04:40:27 +0900

    Feat: Look Aside Pattern to read cache in local

--- src/main/java/com/eum/haetsal/chat/domain/repository/ChatRepository.java
+++ src/main/java/com/eum/haetsal/chat/domain/repository/ChatRepository.java
@@ -1,12 +1,13 @@
 package com.eum.haetsal.chat.domain.repository;
 
 import com.eum.haetsal.chat.domain.model.Message;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.LinkedList;
 
 @Repository
 public interface ChatRepository extends MongoRepository<Message, String>{
-    List<Message> findMessageByChatRoomId(String chatRoomId);
+    LinkedList<Message> findAllByChatRoomIdOrderByCreatedAtDesc(String chatRoomId, Pageable pageable);
 }
