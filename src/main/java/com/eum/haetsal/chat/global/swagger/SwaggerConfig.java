5c992bf2e708662dbe04d64994409bd3b720b2a5
solhee-hwang <solhee0912@kookmin.ac.kr>
2024-05-14 03:55:26 +0900

    chore: swagger 분기 설정

--- src/main/java/com/eum/haetsal/chat/global/swagger/SwaggerConfig.java
+++ src/main/java/com/eum/haetsal/chat/global/swagger/SwaggerConfig.java
@@ -2,15 +2,20 @@ package com.eum.haetsal.chat.global.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(@Value("${openapi.service.url}")String url) {
         return new OpenAPI()
+                .servers(List.of(new Server().url(url)))
                 .info(new Info()
                         .title("EUM Chat API")
                         .description("정릉 이음 어플의 채팅 기능을 제공합니다.")
